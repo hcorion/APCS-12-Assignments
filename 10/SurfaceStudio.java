33374c70987845505b5861f4243a968143470c3b
Zion Nimchuk <zionnimchuk@gmail.com>
2017-02-10 11:21:21 -0800

    Worked more on drawing, nearly done.

--- 10/SurfaceStudio.java
+++ 10/SurfaceStudio.java
@@ -19,24 +19,38 @@ public class SurfaceStudio extends AllInOne
     {
         drawStands(g, x, y);
         drawBase(g, x, y);
+        drawScreen(g, x, y);
     }
 
     private void drawBase(Graphics g, int x, int y)
     {
         g.setColor(Color.gray);
-        g.fillRect(286, 538, 230, 19);
+        g.fillRect(286, 538, 228, 19);
+
         g.fillPolygon(new int[]{293, 507, 514, 285}, new int[]{514,514,538, 538}, 4);
+
         g.setColor(Color.black);
-        g.drawRect(286, 538, 230, 19);
+        g.drawRect(286, 538, 228, 19);
         g.drawPolygon(new int[]{293, 507, 514, 285}, new int[]{514,514,538, 538}, 4);
+    }
+
+    private void drawScreen(Graphics g, int x, int y)
+    {
+        // Draw the border
+        g.setColor(Color.black);
+        g.fillRoundRect(111, 62, 690-111, 460-62, 10, 10);
     }
     private void drawStands(Graphics g, int x, int y)
     {
-        //g.setColor(Color.gray);
-        //g.fillRect(286, 538, 230, 19);
-        //g.fillPolygon(new int[]{293, 507, 514, 285}, new int[]{514,514,538, 538}, 4);
+        // Drawing the left stand.
         g.setColor(Color.black);
-        //g.drawRect(286, 538, 230, 19);
-        g.drawPolygon(new int[]{267, 279, 283, 300, 292, 284, 269}, new int[]{460, 460, 512, 515, 516, 536, 536}, 7);
+        g.drawPolygon(new int[]{267, 279, 283, 300, 292, 294, 269}, new int[]{460, 460, 512, 515, 516, 536, 536}, 7);
+        g.setColor(Color.gray);
+        g.fillPolygon(new int[]{267, 279, 283, 300, 292, 294, 269}, new int[]{460, 460, 512, 515, 516, 536, 536}, 7);
+        // Drawing the right stand.
+        g.setColor(Color.black);
+        g.drawPolygon(new int[]{521, 533, 531, 511, 501, 513, 517}, new int[]{460, 460, 536, 536, 516, 517, 511}, 7);
+        g.setColor(Color.gray);
+        g.fillPolygon(new int[]{521, 533, 531, 511, 501, 513, 517}, new int[]{460, 460, 536, 536, 516, 517, 511}, 7);
     }
 }
